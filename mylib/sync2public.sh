7ff5cd73fd48cd9fecc2943e4ad441e7a3444220
Fengzi21 <32854911+Fengzi21@users.noreply.github.com>
2021-09-25 23:25:52 +0800

    Delete mylib directory

--- mylib/sync2public.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-rsync -auvzP -e "ssh -p 22001" $1 liuyf@159.226.170.164:/public/liuyf/$2
-
--- mylib/sync2storage1.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-rsync -auvzP -e "ssh -p 22022" $1 liuyf@159.226.170.164:~/$2
-
